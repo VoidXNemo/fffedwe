cb81d1614b40f2500c227741a5793115222e6f49
VoidXNemo <62220621+VoidXNemo@users.noreply.github.com>
2020-12-11 16:19:43 -0800

    e

--- commands/eval.js
+++ commands/eval.js
@@ -1,5 +1,7 @@
-const Discord = require("discord.js"),
-      { post } = require("node-superfetch");
+const { MessageEmbed } = require('discord.js');
+const {
+    ownerid
+  } = require('../config.json');
     
 module.exports = {
     name: 'eval',
@@ -10,48 +12,38 @@ module.exports = {
     args: -1,
     catergory: 'Memes/Images Manipulation',
     async execute(message, args, client) {
-        if (!client.config.ownerid.includes(message.author.id)) return;
-  
-        const embed = new Discord.MessageEmbed()
-        .addField("Input", "```js\n" + args.join(" ") + "```");
-        
+        if (message.author.id !== ownerid) return;
+        const embed = new MessageEmbed()
+            .setTitle('Evaluating...')
+        const msg = await message.channel.send(embed);
         try {
-          const code = args.join(" ");
-          if (!code) return message.channel.send("Please include the code.");
-          let evaled;
-          
-          // This method is to prevent someone that you trust, open the secret shit here.
-          if (code.includes(`token`) || code.includes(`TOKEN`) || code.includes("process.env")) {
-            evaled = "No, shut up, what will you do it with the token?";
-          } else {
-            evaled = eval(code);
-          }
-          
-          if (typeof evaled !== "string") evaled = require("util").inspect(evaled, {depth: 0});
-          
-          let output = clean(evaled);
-          if (output.length > 1024) {
-            // If the output was more than 1024 characters, we're gonna export them into the hastebin.
-            const {body} = await post("https://hastebin.com/documents").send(output);
-            embed.addField("Output", `https://hastebin.com/${body.key}.js`).setColor(b19cd9);
-            // Sometimes, the body.key will turn into undefined. It might be the API is under maintenance or broken.
-          } else {
-            embed.addField("Output", "```js\n" + output + "```").setColor(b19cd9)
-          }
-          
-          message.channel.send(embed);
-          
-        } catch (error) {
-          let err = clean(error);
-          if (err.length > 1024) {
-            // Do the same like above if the error output was more than 1024 characters.
-            const {body} = await post("https://hastebin.com/documents").send(err);
-            embed.addField("Output", `https://hastebin.com/${body.key}.js`).setColor("b19cd9");
-          } else {
-            embed.addField("Output", "```js\n" + err + "```").setColor("b19cd9");
-          }
-          
-          message.channel.send(embed);
+            const data = eval(args.join(' ').replace(/```/g, ''));
+            const embed = new MessageEmbed()
+                .setTitle('Output: ')
+                .setDescription(await data)
+                .setColor('b19cd9')
+            await msg.edit(embed)
+            await msg.react('✅')
+            await msg.react('❌')
+            const filter = (reaction, user) => (reaction.emoji.name === '❌' || reaction.emoji.name === '✅') && (user.id === message.author.id);
+            msg.awaitReactions(filter, { max: 1 })
+                .then((collected) => {
+                    collected.map((emoji) => {
+                        switch (emoji._emoji.name) {
+                            case '✅':
+                                msg.reactions.removeAll();
+                                break;
+                            case '❌':
+                                msg.delete()
+                                break;
+                        }
+                    })
+                })
+        } catch (e) {
+            const embed = new MessageEmbed()
+                .setTitle('An Error has occured')
+            return await msg.edit(embed);
+
         }
-      }
+    }
 }
