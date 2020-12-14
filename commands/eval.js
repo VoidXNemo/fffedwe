a917176e4999f81fd320d814cfba72a798d534d1
VoidXNemo <62220621+VoidXNemo@users.noreply.github.com>
2020-12-14 09:20:07 -0800

    Update eval.js

--- commands/eval.js
+++ commands/eval.js
@@ -42,6 +42,8 @@ module.exports = {
         } catch (e) {
             const embed = new MessageEmbed()
                 .setTitle('An Error has occured')
+                .setDescription(e)
+                .setColor('RED')
             return await msg.edit(embed);
 
         }
